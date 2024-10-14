074e7fbd32f352ac116af13d6a41b693aa489890
vinicius-omelo <viniciuss.uee@gmail.com>
2024-10-14 19:42:14 -0300

    commit

--- src/ndex.js
+++ src/ndex.js
@@ -3,7 +3,7 @@ const sons = {
   S: "sounds_clap.wav",
   D: "sounds_hihat.wav",
   F: "sounds_kick.wav",
-  G: "sounds_opebhat.wav",
+  G: "sounds_openhat.wav",
   H: "sounds_ride.wav",
   J: "sounds_snare.wav",
   K: "sounds_tink.wav",
@@ -17,3 +17,18 @@ const criarDiv = (texto) => {
   div.id = texto;
   document.getElementById("container").appendChild(div);
 };
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+const tocarSom = (letra) => {
+  const audio = new Audio(`./src/sounds/${sons[letra]}`);
+  audio.play();
+};
+const ativarDiv = (evento) => {
+  const letra = evento.target.id;
+  tocarSom(letra);
+};
+
+exibir(sons);
+
+document.getElementById("container").addEventListener("click", ativarDiv);
